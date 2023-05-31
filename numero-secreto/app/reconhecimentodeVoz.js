114bd3e7773c332d46337cc1b04dccb4d9ff5906
Matheus-D <59287764+Matheus-D@users.noreply.github.com>
2023-05-31 14:08:40 -0400

    Projeto Finalizado

--- numero-secreto/app/reconhecimentodeVoz.js
+++ numero-secreto/app/reconhecimentodeVoz.js
@@ -13,11 +13,13 @@ recognition.addEventListener('result', onSpeak)
 function onSpeak(e) {
      chute = e.results[0][0].transcript
      exibeChuteNaTela(chute)
+     verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
         <div>Você disse</div>
-        <span class=”box”>${chute}</span>>
+        <span class=”box”>${chute}</span>
         `
 }
+recognition.addEventListener('end', () =>recognition.start())
